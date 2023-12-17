b4aa3b6a6b5749a85dbe87d85389116a8a19e8d4
qgsoriano1 <qgsoriano1@tip.edu.ph>
2023-12-17 15:52:38 +0800

    update

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 def load_image(file_path):
     try:
-        print(f"Loading image from: {file_path}")
         image = sitk.ReadImage(file_path)
         return image
     except Exception as e:
@@ -11,7 +10,6 @@ def load_image(file_path):
         return None
 
 def display_image_slice(image, slice_number, title):
-    # Get the 2D slice from the 3D volume
     slice_2d = sitk.GetArrayViewFromImage(image)[slice_number, :, :]
     plt.imshow(slice_2d, cmap='gray')
     plt.title(title)
@@ -23,25 +21,17 @@ def smooth_image(image, sigma=1.0):
     return smoothed_image
 
 def segment_image(image):
-    # Example: Threshold-based segmentation
     binary_image = sitk.BinaryThreshold(image, lowerThreshold=100, upperThreshold=500)
     return binary_image
 
 def main():
-    # Replace the existing file path with the desired one
-    file_path = r'C:\Users\GSori\OneDrive\Documents\4thYear\DSPA\MRITechniques\assets\fsl-open-dev_sub-001_T1w.nii.gz'
-    # Load MRI image
+    file_path = r'C:\Users\GSori\OneDrive\Documents\4thYear\DSPA\MRITechniques\assets\wash-120_sub-001_T1w.nii.gz'
     mri_image = load_image(file_path)
 
     if mri_image is not None:
-        # Display a slice of the original image
         display_image_slice(mri_image, slice_number=100, title='Original MRI Image')
-
-        # Smooth the image
         smoothed_image = smooth_image(mri_image)
         display_image_slice(smoothed_image, slice_number=100, title='Smoothed MRI Image')
-
-        # Segment the image
         segmented_image = segment_image(mri_image)
         display_image_slice(segmented_image, slice_number=100, title='Segmented MRI Image')
 
